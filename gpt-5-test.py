f53d729ab2968b489bb5449a5ffbea3bfa7b175b
Saurabh Seth <6327636+takshila@users.noreply.github.com>
2025-10-21 19:56:02 -0700

    Create gpt-5-test.py

---
+++ gpt-5-test.py
@@ -0,0 +1,24 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-5"
+token = os.environ["GITHUB_TOKEN"]
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage(""),
+        UserMessage("What is the capital of France?"),
+    ],
+    model=model
+)
+
+print(response.choices[0].message.content)
+
